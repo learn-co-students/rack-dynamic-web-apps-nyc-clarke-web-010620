efa8e9a84c18667f2a67111c8c92b5f4fcd3c3b9
Steven Zhu <stevenzhu03@gmail.com>
2020-01-26 22:11:36 -0500

    Done.

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,7 @@ class Application
     resp.write "#{num_3}\n"
  
     if num_1==num_2 && num_2==num_3
-      res.write "You Win"
+      resp.write "You Win"
     else
       resp.write "You Lose"
     end
